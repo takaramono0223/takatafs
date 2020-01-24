ede3ec06329e5ddaf34a1cd59345f0b9965a38f4
An sohee <qmffhd@gmail.com>
2020-01-25 01:00:00 +0900

    로그인 db연동시도

--- login.php
+++ login.php
@@ -11,8 +11,8 @@
     <link rel="stylesheet" href="../css/style.css">
     <title></title>
   </head>
-  <body>
 
+  <body>
     <div class="row pt-5">
       <!-- 좌측여백 -->
       <div class="col-2">
@@ -20,16 +20,14 @@
       <!-- 본문 -->
       <div class="col-8">
         <!-- 우상단메뉴 -->
+
+
         <div class="col-12" style="position:relative; text-align:right;">
           <a href="index.php">HOME</a> <a href="login.php">LOGIN</a> <a href="register.php">JOIN</a>
         </div>
         <!-- 로고 -->
         <div class="col-12" style="position:relative; text-align:center; width:100%;">
-<<<<<<< HEAD
           <a href="index.php"><img src="img/logo.jpg"></a>
-=======
-          <img src="img/logo.jpg">
->>>>>>> 016e54cf0ceb473d661e4ce555bc78578ecdd0c2
         </div>
         <hr>
         <!-- 메뉴바 -->
@@ -39,47 +37,29 @@
           <a class="nav-item nav-link disabled" href="#" tabindex="-1" aria-disabled="true">줄</a>
         </nav>
         <hr>
+              <?php
+
+              if(!isset($_SESSION['loginornot'])){ //세션값이 없을때=로그인전상태
+                ?>
                 <div class="row">
-<<<<<<< HEAD
-<!-- 로그인안에 좌측여백 -->
+                <!-- 로그인안에 좌측여백 -->
                   <div class="col-2"></div>
-=======
                   <div class="col-sm-4">
                     <div class="card-group">
-                      <div class="card-body">
-                        <h4 class="card-title">로그인하기</h4>
+            <div class="card-body" method="POST" action="loging.php">
+              <!-- // POST방식으로 loging.php로 입력받은 데이터를 전송하는 태그 -->
+              <?php
+            }else{ //세션값이 존재 = "로그인된 상태"
+              echo "로그인중입니다.";
+            }
+            ?>
+                        <h4>로그인하기</h4>
                         <div class="form-group">
                           <small id="IDHelp" class="form-text text-muted">가입하신 아이디와 비밀번호를 입력해주세요.</small>
                           <label for="IDInPut"></label>
-                          <input type="text" class="form-control col-md-8" id="IDInPut" placeholder="아이디">
+                          <input type="text" class="form-control col-md-8" name="userid" id="IDInPut" placeholder="아이디">
                           <label for="PWInPut"></label>
-                          <input type="password" class="form-control col-md-8" id="PWInPut" placeholder="비밀번호">
-                          <small id="PWHelp" class="text-muted">비밀번호는 최대 20자까지 가능</small>
-                         </div>
-                        <div class="form-group form-check">
-                          <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                          <label class="form-check-label" for="check1">아이디 저장</label>
-                        </div>
-                        <a href="#" class="btn btn-primary btn-lg col-md-8">로그인</a>
-                      </div>
-                    </div>
-                  </div>
->>>>>>> 016e54cf0ceb473d661e4ce555bc78578ecdd0c2
-
-                  <div class="col-sm-4">
-                    <div class="card-group">
-
-<<<<<<< HEAD
-
-
-                      <div class="card-body"  action="login_service.php" method="POST" >
-                        <h4 class="card-title">로그인하기</h4>
-                        <div class="form-group">
-                          <small id="IDHelp" class="form-text text-muted">가입하신 아이디와 비밀번호를 입력해주세요.</small>
-                          <label for="IDInPut"></label>
-                          <input type="text" class="form-control col-md-8" id="IDInPut" placeholder="아이디">
-                          <label for="PWInPut"></label>
-                          <input type="password" class="form-control col-md-8" id="PWInPut" placeholder="비밀번호">
+                          <input type="password" class="form-control col-md-8" name="userpw" id="PWInPut" placeholder="비밀번호">
                           <small id="PWHelp" class="text-muted">비밀번호는 최대 20자까지 가능</small>
                          </div>
 
@@ -88,21 +68,16 @@
                           <label class="form-check-label" for="check1">아이디 저장</label>
                         </div>
 
-                        <a href="#" class="btn btn-primary btn-lg col-md-8">로그인</a>
+                        <a href="loging.php" class="btn btn-primary btn-lg col-md-8">로그인</a>
                       </div>
                     </div>
                   </div>
 
-
-
-=======
->>>>>>> 016e54cf0ceb473d661e4ce555bc78578ecdd0c2
                   <div class="col-sm-4">
                     <div class="card-group">
                       <div class="card-body">
-                        <h4 class="card-title">회원가입하기</h4>
+                        <h4>회원가입하기</h4>
                         <small id="PWHelp" class="text-muted">회원가입하시고 편리하게 이용하세요!</small>
-<<<<<<< HEAD
                         <br><br><a href="register.php" class="btn btn-primary btn-lg col-md-8">회원가입하러가기</a>
                       </div>
                     </div>
@@ -113,31 +88,24 @@
                 <hr>
 
 
-
-=======
-                        <br><br><a href="#" class="btn btn-primary btn-lg col-md-8">회원가입하러가기</a>
-                      </div>
-                    </div>
-                  </div>
-                </div>
->>>>>>> 016e54cf0ceb473d661e4ce555bc78578ecdd0c2
-        <!-- 하단메뉴 -->
-        <div class="row bg-light p-3">
-          <div class="col-8" style="text: seocondary;">
-            상호명 : 다나까피싱<br>
-            주소 : 서울특별시 영등포구 가마산로 315 1층<br>
-            통신판매업신고 : 제 2015-서울영등포-1455호 / 사업자등록번호 : 108-13-74543<br>
-            대표전화 : 02-846-9895 / FAX : 070-7779-9895<br>
-            Copyright © danakafishing. All rights reserved.
-          </div>
-          <div class="col-4" style="position:relative; text-align:center; width:100%;">
-            <img src="img/logo1.jpg">
+        <!-- 하단고정 -->
+        <div id="footer">
+          <div class="row bg-light">
+            <div class="col-10">
+              상호명 : 다나까피싱 / 대표이사 : 임연식<br>
+              주소 : 서울특별시 영등포구 가마산로 315 1층 / 사업자등록번호 : 108-13-74543<br>
+              통신판매업신고 : 제 2015-서울영등포-1455호 / 대표전화 : 02-846-9895 / FAX : 070-7779-9895<br>
+              개인정보책임자 : 임연식(dawoo63@hanmail.net)<br>
+              Copyright © danakafishing. All rights reserved.
+            </div>
+            <div class="col-2">
+              <img src="img/logo1.jpg">
+            </div>
           </div>
         </div>
-      </div>
-      <!-- 우측여백 -->
-      <div class="col-2">
-      </div>
-    <br>
-  </body>
-</html>
+        <!-- 우측여백 -->
+        <div class="col-2">
+        </div>
+        <br>
+        </body>
+        </html>
--- loging.php
+++ loging.php
@@ -1,20 +1,50 @@
 <?php
- $host = 'localhost';
- $user = 'root';
- $pw = '';
- $dbName = 'register';
- $mysqli = new mysqli($host, $user, $pw, $dbName);
+//MySQL연결
+$db = new mysqli('localhost', 'root', '');
+$db->query("set names UTF8");
 
- $id = $_POST['id'];
- $$password = $_POST['pw'];
 
- if($id == 'guest' && $pw =='guest'){
-  header('location:index.php'); //바로이동
- }else {
-   session_start();
-   $_SESSION['msg'] = "아이디 또는 암호가 일치하지 않습니다.";
-   header('location:login.php');
- }
+
+session_start();
+
+$userid=$_POST['userid'];
+$userpw=$_POST['userpw'];
+
+$sql = "SELECT * FROM user_info WHERE userid = '{$userid}' AND userpw = md5('{$userpw}') ";
+$result = mysqli_query($db,$sql);
+$num = mysqli_num_rows($result);
+
+
+if($num>0){ // 아이디와 패스워드가 일치하는 데이터가 검색된경우
+  // user_info테이블에서 입력받은 아이디와 일치한 데이터가 있는지 검색.
+  // user_info테이블 = 접속해 있는 계정을 저장하는 테이블
+
+  $sql = "SELECT*FROM user_info WHERE $userid = '{$userid}'";
+  $result = mysqli_query($sql);
+  $num = mysqli_num_rows($result);
+
+  if($num>0){ //user_info테이블에 데이터가 존재 = 이미 로그인된 사용자
+    echo"<script> alert('이미 로그인 한 사용자입니다. ');</script>";
+  }else{
+//로그인 아직 안한 사용자, 이제 로그인됨.
+//user_info테이블에 사용자 정보 입력(insert)
+
+$sess_id = session_id();
+$sql="INSERT INTO user_info VALUE('$userid', '$sess_id')";
+//아이디, 세션아이디 값을 테이블에 저장
+// 다음 로그인시 해당테이블에 더이터 존재유무를 통해 로그인중인지 아닌지를 판단.
+$ret=mysqli_query($sql);
+
+$_SESSION[userid]=$userid;
+$_SESSION[loginornot]=1;
+echo "<script> alert('로그인 되었습니다.');</script>";
+
+  }
+}else {// 테이블에 입력받은 아이디와 패스워드가 일치하지않음
+
+echo "<script> alert('아이디 또는 패스워드가 올바르지 않습니다.');</script>";
+
+}
 
 
 
