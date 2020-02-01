c536d7a8d67d6d7d69494e1df5c302fce002d473
An sohee <qmffhd@gmail.com>
2020-02-01 13:31:47 +0900

    카트화면수정

--- cart.php
+++ cart.php
@@ -13,28 +13,21 @@
   </head>
 
   <body>
-    <div class="row pt-5">
-      <!-- 좌측여백 -->
-      <div class="col-2">
-      </div>
-      <!-- 본문 -->
-      <div class="col-8">
-        <!-- 우상단메뉴 -->
-        <div class="col-12" style="position:relative; text-align:right;">
-          <a href="index.php">HOME</a> <a href="login.php">LOGIN</a> <a href="register.php">JOIN</a>
-        </div>
-        <!-- 로고 -->
+        <div class="row pt-5">
+        <!-- 좌측여백 -->
+          <div class="col-2">
+          </div>
+        <!-- 본문 -->
+          <div class="col-8">
+            <!-- 우상단메뉴 -->
+                  <div class="col-12" style="position:relative; text-align:right;">
+                    <a href="index.php">HOME</a> <a href="login.php">LOGIN</a> <a href="register.php">JOIN</a>
+                  </div>
+        <!-- 로고/상단메뉴 -->
         <div class="col-12" style="position:relative; text-align:center; width:100%;">
           <a href="index.php"><img src="img/logo.jpg"></a>
         </div>
-        <hr>
-        <!-- 메뉴바 -->
-        <nav class="nav nav-pills nav-fill">
-          <a class="nav-item nav-link disabled" href="#">낚시대</a>
-          <a class="nav-item nav-link disabled" href="#">릴</a>
-          <a class="nav-item nav-link disabled" href="#" tabindex="-1" aria-disabled="true">줄</a>
-        </nav>
-        <hr>
+
 <!-- 상품상세 -->
     <?php
       session_start();
